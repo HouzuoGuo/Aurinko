3425737c967776fea4a561cb1198c15f913f6009
HouzuoGuo <guohouzuo@gmail.com>
2012-09-03 22:12:49 +1000

    rewriting skiplist

--- src/Aurinko/skiplist.clj
+++ src/Aurinko/skiplist.clj
@@ -3,105 +3,84 @@
   (:import (java.nio.channels FileChannel FileChannel$MapMode)
            (java.nio MappedByteBuffer)))
 
-(def ^:const FILE-HDR (int 8)) ; file header: number of levels, chance
-(def ^:const PTR-SIZE (int 4)) ; every pointer in the file is an integer
-(def ^:const NODE     (int 4)) ; every node has integer key and value
+(def ^:const FILE-HDR (int 8))  ; file header: number of levels, chance
+(def ^:const PTR-SIZE (int 4))  ; every pointer in the file is an integer
+(def ^:const NODE     (int 4))  ; every node has integer key and value
+(def ^:const NIL      (int -1)) ; nil pointer
 
 (defprotocol SkipListP
-  (rand-lvl [this])
-  (head     [this])
-  (at-node  [this n])
-  (node     [this n])
-  (cut-lvl  [this lvl begin-from v])
-  (cutlist  [this v])
-  (v-after  [this v node top-lvl])
-  (store    [this val])
-  (get      [this val])
-  (x        [this val]))
+  (at [this node-num])
+  (node-at [this node-num])
+  (cut-lvl [this v lvl begin-from])
+  (insert [this v]))
 
 (deftype SkipList [path levels P fc ^{:unsynchronized-mutable true} file cmp-fun] SkipListP
-  (rand-lvl [this]
-            (loop [lvl 1]
-              (if (and (< lvl levels) (< (Math/random) P))
-                (recur (inc lvl))
-                lvl)))
-  (head [this]
-        (.position file FILE-HDR)
-        (vec (map (.getInt file) (range levels))))
-  (at-node [this n]
-           (.position file (+ FILE-HDR
-                             (* n (+ (* PTR-SIZE levels) NODE)))))
-  (node [this n]
-        (at-node this n)
-        {:n n :v (.getInt file) :lvls (vec (map (fn [_] (.getInt file)) (range levels)))})
-  (cut-lvl [this lvl begin-from v]
-           (loop [matches (transient [])
-                  prev-n begin-from
-                  n begin-from]
-             (let [node    (node this n)
-                   lvl-ptr (nth (:lvls node) lvl)
-                   cmp     (cmp-fun (:v node) v)]
-               (cond
-                 (= cmp -1)
-                 (if (not= lvl-ptr -1)
-                   (recur matches n lvl-ptr)
-                   {:n n :matches (persistent! matches)})
-                 (= cmp 0)
-                 (if (not= lvl-ptr -1)
-                   (recur (conj! matches (assoc node :prev-n prev-n)) n lvl-ptr)
-                   {:n n :matches (persistent! (conj! matches (assoc node :prev-n prev-n)))})
-                 (= cmp 1)
-                 {:n prev-n :matches (persistent! matches)}))))
-  (cutlist [this v]
-           (loop [lvl (dec levels)
-                  cut (transient [])
-                  curr-node 0]
-             (if (> lvl -1)
-               (let [cut-lvl (cut-lvl this lvl curr-node v)]
-                 (conj! cut cut-lvl)
-                 (recur (dec lvl) cut (:n cut-lvl)))
-               (persistent! cut))))
-  (v-after [this v node top-lvl]
-           (let [new-node-pos (.limit file)
-                 new-node-num (quot (- (.limit file) FILE-HDR) (+ NODE (* levels PTR-SIZE)))]
-             (set! file (.map fc FileChannel$MapMode/READ_WRITE
-                          0 (+ (.limit file) (+ NODE (* PTR-SIZE levels)))))
-             (.position file new-node-pos)
-             (.putInt file v)
-             (doseq [lvl (range top-lvl)]
-               (at-node this node)
-               (let [lvl-ptr-pos (+ (.position file) NODE (* PTR-SIZE lvl))]
-                 (.position file lvl-ptr-pos)
-                 (let [lvl-ptr (.getInt file)]
-                   (.putInt (.position file lvl-ptr-pos) new-node-num)
-                   (.putInt (.position file (+ new-node-pos NODE (* PTR-SIZE lvl))) lvl-ptr))))))
-  (store [this val]
-         (let [cut (cutlist this val)
-               match (last (get this val))]
-           (if (nil? match)
-             (v-after this val (:n (last cut)) (rand-lvl this))
-             (v-after this val (:n match) (- levels (count (filter zero? (:lvls match))))))))
-  (get [this v]
-       (flatten (for [cut-lvl (cutlist this v)]
-                  (filter #(not (empty? %)) (:matches cut-lvl)))))
-  (x [this val]
-     (when-let [node (first (filter #(= (:v %) val) (get this val)))]
-       (at-node this (:prev-n node))
-       (.position file (+ (.position file) NODE))
-       (doseq [lvl-ptr (:lvls node)]
-         (.putInt file lvl-ptr)))))
+  (at [this node-num]
+      (.position file (+ FILE-HDR (* node-num (+ NODE (* PTR-SIZE levels))))))
+  (node-at [this node-num]
+           (at this node-num)
+           {:n node-num :v (.getInt file) :lvls (vec (map (fn [_] (.getInt file)) (range levels)))})
+  (cut-lvl [this v lvl begin-from]
+           (loop [curr-node-num begin-from
+                  prev-node (node-at this begin-from)
+                  matches   (transient [])]
+             (if (= curr-node-num NIL)
+                 {:prev prev-node :node prev-node :matches (persistent! matches)}
+                 (let [curr-node (node-at this curr-node-num)
+                       next-node-num (nth (:lvls curr-node) lvl)
+                       cmp-result (cmp-fun (:v curr-node) v)]
+                   (cond
+                     (= cmp-result -1)
+                     (recur next-node-num curr-node matches)
+                     (= cmp-result 0)
+                     (recur next-node-num curr-node (conj! matches curr-node))
+                     (= cmp-result 1)
+                     {:prev prev-node :node curr-node :matches (persistent! matches)})))))
+  (insert [this v]
+          (prn "insert" v)
+          (let [empty-list? (= (.limit file) FILE-HDR)
+                new-node-pos (.limit file)
+                new-node-num (quot (- (.limit file) FILE-HDR) (+ NODE (* levels PTR-SIZE)))]
+            (set! file (.map fc FileChannel$MapMode/READ_WRITE
+                         0 (+ (.limit file) NODE (* PTR-SIZE levels))))
+            (if empty-list?
+              (do
+                (.position file FILE-HDR)
+                (.putInt file v)
+                (doseq [i (range levels)]
+                  (.putInt file NIL)))
+              (let [top-lvl (loop [lvl 0]
+                              (if (and (< lvl (dec levels)) (< (Math/random) P))
+                                (recur (inc lvl))
+                                lvl))]
+                (.position file new-node-pos)
+                (.putInt file v)
+                (doseq [v (range levels)]
+                  (.putInt file NIL))
+                (loop [lvl top-lvl
+                       node-num 0]
+                  (when (> lvl -1)
+                    (let [lvl-cut (cut-lvl this v lvl node-num)
+                          last-lvl-node (:n (:node lvl-cut))]
+                      (at this last-lvl-node)
+                      (let [ptr-pos (+ (.position file) NODE (* PTR-SIZE lvl))
+                            old-node-num (do (.position file ptr-pos) (.getInt file))]
+                        (prn "last node" (:node lvl-cut) "at level" lvl "old pointer" old-node-num "new pointer" new-node-num)
+                        (.position file ptr-pos)
+                        (.putInt file new-node-num)
+                        (.position file (+ new-node-pos NODE (* PTR-SIZE lvl)))
+                        (.putInt file old-node-num))
+                      (recur (dec lvl) last-lvl-node)))))))))
        
 (defn open [path cmp-fun]
   (let [fc   (.getChannel (RandomAccessFile. path "rw"))
         file (.map fc FileChannel$MapMode/READ_WRITE 0 (.size fc))]
     (SkipList. path (int (.getInt file))
-              (/ (int (.getInt file))) fc file cmp-fun)))
+              (/ (.getInt file)) fc file cmp-fun)))
 
 (defn new [path levels chance cmp-fun]
   (let [fc   (.getChannel (RandomAccessFile. path "rw"))
-        file (.map fc FileChannel$MapMode/READ_WRITE 0 (+ FILE-HDR NODE (* PTR-SIZE levels)))]
+        file (.map fc FileChannel$MapMode/READ_WRITE 0 FILE-HDR)]
     (.putInt file levels)
     (.putInt file chance)
-    (doseq [l (range (inc levels))]
-      (.putInt file -1))
     (open path cmp-fun)))
