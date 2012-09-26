0ca7389647e83069eca5e1ad45e983cd09531d47
HouzuoGuo <guohouzuo@gmail.com>
2012-09-26 23:03:50 +1000

    project.clj update to v0.3

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject Aurinko "0.1"
+(defproject Aurinko "0.3"
   :description "A networked document database engine implementation in Clojure programming language"
   :url "https://github.com/HouzuoGuo/Aurinko"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Simplified BSD License"
+            :url "http://www.freebsd.org/copyright/freebsd-license.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :main Aurinko.core
   :warn-on-reflection true
